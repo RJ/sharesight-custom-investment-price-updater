6c31b8d915cfac83b5d0f2e85c7fbec4961caec6
RJ <rj@metabrew.com>
2022-01-24 13:19:19 +0000

    tidy

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,11 @@ use reqwest::header;
 
 use serde::Deserialize;
 
-use serde_json;
 use serde_json::json;
 
 use chrono::{prelude::*, Duration};
 
-use clap::{arg, App, Arg, AppSettings};
-
+use clap::{arg, App, AppSettings, Arg};
 
 #[derive(Debug)]
 struct State {
@@ -34,7 +32,6 @@ struct AuthResponse {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let args = App::new("sharesight")
         .about("Custom price updater for sharesight portfolios")
         .setting(AppSettings::SubcommandRequiredElseHelp)
@@ -79,7 +76,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         client: None,
     };
 
-
     if !do_auth(&mut creds) {
         process::exit(1);
     }
@@ -96,10 +92,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     panic!("Invalid scraping code");
                 }
                 let (date, price) = get_vanguard_global_small_cap_index_fund_price();
-                println!("{code}\t{date}\t{price}");
+                println!("{}\t{}\t{}", code, date, price);
                 Ok(())
             }
-        },
+        }
         Some(("list", _sub_m)) => {
             let custom_investments = get_custom_investments(&creds);
             // println!("id\tcode\tname");
@@ -107,11 +103,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("{}\t{}\t{}", ci.id, ci.code, ci.name);
             }
             Ok(())
-        },
+        }
         Some(("update", sub_m)) => {
             // use ID lets us know the user supplied the internal sharesight custom investment id themselves
             let investment_id: u32 = if sub_m.is_present("use-id") {
-                sub_m.value_of("investment").unwrap().parse::<u32>().unwrap()
+                sub_m
+                    .value_of("investment")
+                    .unwrap()
+                    .parse::<u32>()
+                    .unwrap()
             } else {
                 find_custom_investment_id(&creds, sub_m.value_of("investment").unwrap())
                     .expect("Can't find investment id for this code")
@@ -120,13 +120,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // verify date format
             let date_str = sub_m.value_of("date").unwrap();
             if NaiveDate::parse_from_str(date_str, "%Y-%m-%d").is_err() {
-                panic!("Invalid date format, expecting YYYY-MM-DD, got '{}'", date_str);
+                panic!(
+                    "Invalid date format, expecting YYYY-MM-DD, got '{}'",
+                    date_str
+                );
             }
 
-            let price = sub_m.value_of("price").unwrap().parse::<f64>()
-                                .expect("Invalid price format, expecting float");
-            
-            println!("add_custom_investment_price {} {} {}", investment_id, date_str, price);
+            let price = sub_m
+                .value_of("price")
+                .unwrap()
+                .parse::<f64>()
+                .expect("Invalid price format, expecting float");
+
+            println!(
+                "add_custom_investment_price {} {} {}",
+                investment_id, date_str, price
+            );
 
             if add_custom_investment_price(&creds, investment_id, price, date_str) {
                 println!("Success");
@@ -135,21 +144,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 eprintln!("Failed to put custom price");
                 Err("Fail".into())
             }
-        },
-        _ => {
-            Err("Missing subcommand".into())
         }
-    }   
+        _ => Err("Missing subcommand".into()),
+    }
 }
 
 fn find_custom_investment_id(creds: &State, code: &str) -> Option<u32> {
-    let custom_investments = get_custom_investments(&creds);
+    let custom_investments = get_custom_investments(creds);
     for ci in custom_investments {
         if ci.code == code {
-            return Some(ci.id)
+            return Some(ci.id);
         }
     }
-    None   
+    None
 }
 
 #[derive(Deserialize, Debug)]
@@ -165,7 +172,6 @@ struct CustomInvestment {
     id: u32,
 }
 
-
 // https://www.vanguardinvestor.co.uk/investments/vanguard-global-small-cap-index-fund-gbp-acc/price-performance
 // api call this pages makes is:
 // $ curl "https://api.vanguard.com/rs/gre/gra/1.7.0//datasets/urd-product-port-specific-price-history.json?vars=portId%3A9158%2CissueType%3AS%2CstartDate%3A2022-01-03%2CendDate%3A2022-01-13&callback=angular.callbacks._j"
@@ -179,12 +185,14 @@ struct HistoricVanguardFundPrice {
 
 fn get_vanguard_global_small_cap_index_fund_price() -> (String, f64) {
     let today = Local::now().format("%Y-%m-%d").to_string();
-    let ten_ago = (Utc::now() - Duration::days(10)).format("%Y-%m-%d").to_string();
+    let ten_ago = (Utc::now() - Duration::days(10))
+        .format("%Y-%m-%d")
+        .to_string();
     let url = format!("https://api.vanguard.com/rs/gre/gra/1.7.0//datasets/urd-product-port-specific-price-history.json?vars=portId%3A9158%2CissueType%3AS%2CstartDate%3A{}%2CendDate%3A{}", ten_ago, today);
     // println!("{}", url);
     let prices: Vec<HistoricVanguardFundPrice> =
         reqwest::blocking::get(url).unwrap().json().unwrap();
-    
+
     let hp = &prices[0];
     let mut date = hp.date.clone();
     date.truncate(10);
@@ -193,27 +201,38 @@ fn get_vanguard_global_small_cap_index_fund_price() -> (String, f64) {
 }
 
 fn get_custom_investments(creds: &State) -> Vec<CustomInvestment> {
-    let h: CustomInvestments =
-        creds.client.as_ref().unwrap()
+    let h: CustomInvestments = creds
+        .client
+        .as_ref()
+        .unwrap()
         .get("https://api.sharesight.com/api/v3/custom_investments.json")
         .send()
         .unwrap()
         .json()
-        .unwrap()
-        ;
+        .unwrap();
     h.custom_investments
 }
 
 fn add_custom_investment_price(creds: &State, investment_id: u32, price: f64, date: &str) -> bool {
     // println!("Setting custom investment price for {} to {} @ {}", ci.name, price, date);
-    let mut body: HashMap::<String, serde_json::Value> = HashMap::new();
+    let mut body: HashMap<String, serde_json::Value> = HashMap::new();
     body.insert("last_traded_on".into(), json!(date));
     body.insert("last_traded_price".into(), json!(price));
 
     // let url = format!("http://localhost:9999/api/v3/prices/{}.json", ci.id);
-    let url = format!("https://api.sharesight.com/api/v3/custom_investment/{}/prices.json", investment_id);
+    let url = format!(
+        "https://api.sharesight.com/api/v3/custom_investment/{}/prices.json",
+        investment_id
+    );
     // println!("url = {} body = {:?}", url, body);
-    let r = creds.client.as_ref().unwrap().post(url).json(&body).send().unwrap();
+    let r = creds
+        .client
+        .as_ref()
+        .unwrap()
+        .post(url)
+        .json(&body)
+        .send()
+        .unwrap();
     r.status().is_success()
 }
 
@@ -221,15 +240,15 @@ fn do_auth(creds: &mut State) -> bool {
     let params = [
         ("grant_type", "client_credentials"),
         ("client_id", &creds.client_id),
-        ("client_secret", &creds.client_secret)
+        ("client_secret", &creds.client_secret),
     ];
 
     let client = reqwest::blocking::Client::new();
-    let resp = client.post("https://api.sharesight.com/oauth2/token")
+    let resp = client
+        .post("https://api.sharesight.com/oauth2/token")
         .form(&params)
         .send()
-        .expect("Error during http auth")
-        ;
+        .expect("Error during http auth");
     if !resp.status().is_success() {
         println!("Error during auth {:#?}", resp);
         false
@@ -237,21 +256,19 @@ fn do_auth(creds: &mut State) -> bool {
         let j: AuthResponse = resp.json().expect("Invalid auth response json");
         creds.bearer_token = Some(j.access_token.clone());
 
-        
         let mut headers = header::HeaderMap::new();
-        let mut auth_val = header::HeaderValue::from_str(
-            format!("Bearer {}", j.access_token.clone()).as_str()).unwrap();
+        let mut auth_val =
+            header::HeaderValue::from_str(format!("Bearer {}", j.access_token).as_str()).unwrap();
         auth_val.set_sensitive(true);
         headers.insert("Authorization", auth_val);
 
         // println!("Authorization: Bearer {}", j.access_token.clone());
 
         let client = reqwest::blocking::Client::builder()
-        .user_agent("RJ's tool for updating custom investment price")
-        .default_headers(headers)
-        .build()
-        .expect("Couldn't build http client")
-        ;
+            .user_agent("github.com/RJ/sharesight-custom-investment-price-updater")
+            .default_headers(headers)
+            .build()
+            .expect("Couldn't build http client");
         creds.client = Some(client);
 
         true
